488ccf5fcfa21348a90b98f5edcabf276990120b
tgrundtvig <tgrundtvig@gmail.com>
2016-03-15 13:53:49 +0100

    Version 0.1.2 Location has equals and hashcode

--- src/aiantwars/impl/Location.java
+++ src/aiantwars/impl/Location.java
@@ -93,4 +93,42 @@ public class Location implements ILocationInfo
     {
         this.ant = ant;
     }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 41 * hash + this.x;
+        hash = 41 * hash + this.y;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this == obj)
+        {
+            return true;
+        }
+        if(obj == null)
+        {
+            return false;
+        }
+        if(getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if(this.x != other.x)
+        {
+            return false;
+        }
+        if(this.y != other.y)
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
